4d27fbb7033e1d0ee844721bf03c1170af910fff
Ester79 <esterramoscuervo@hotmail.com>
2022-07-14 09:48:32 +0200

    create interfaces

---
+++ src/app/interfaces/event.interface.ts
@@ -0,0 +1,14 @@
+export interface Event {
+  event: {
+    id: string;
+    title: string;
+    subtitle: string;
+    image: string;
+  },
+  sessions: Calendar[];
+}
+
+export interface Calendar {
+  date: string;
+  availability: string;
+}
