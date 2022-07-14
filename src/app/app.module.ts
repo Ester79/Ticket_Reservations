39503ae03f70d76163ae912859ace2439102d6c0
Ester79 <esterramoscuervo@hotmail.com>
2022-07-14 08:58:02 +0200

    create components / clean app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CartComponent } from './cart/cart.component';
+import { DetailsComponent } from './details/details.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    DetailsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
